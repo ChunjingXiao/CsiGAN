30215d38ad5e3131fcb7481c1130b268768f6e65
Chunjing Xiao <chunjingxiao@gmail.com>
2019-03-17 21:23:00 +0800

    Add files via upload

---
+++ 1SemiSupGAN/CycleGANmodule.py
@@ -0,0 +1,254 @@
+from __future__ import division
+import tensorflow as tf
+#from ops import *
+#from utils import *
+
+
+def discriminator(image, options, reuse=False, name="discriminator"):
+
+    with tf.variable_scope(name):
+        # image is 256 x 256 x input_c_dim
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+        else:
+            assert tf.get_variable_scope().reuse is False
+
+        h0 = lrelu(conv2d(image, options.df_dim, name='d_h0_conv'))
+        # h0 is (128 x 128 x self.df_dim)
+        h1 = lrelu(instance_norm(conv2d(h0, options.df_dim*2, name='d_h1_conv'), 'd_bn1'))
+        # h1 is (64 x 64 x self.df_dim*2)
+        h2 = lrelu(instance_norm(conv2d(h1, options.df_dim*4, name='d_h2_conv'), 'd_bn2'))
+        # h2 is (32x 32 x self.df_dim*4)
+        h3 = lrelu(instance_norm(conv2d(h2, options.df_dim*8, s=1, name='d_h3_conv'), 'd_bn3'))
+        # h3 is (32 x 32 x self.df_dim*8)
+        h4 = conv2d(h3, 1, s=1, name='d_h3_pred')
+        # h4 is (32 x 32 x 1)
+        return h4
+
+
+def generator_resnet(image, options, reuse=False, name="generator"):
+
+    with tf.variable_scope(name):
+        # image is 256 x 256 x input_c_dim
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+        else:
+            assert tf.get_variable_scope().reuse is False
+
+        def residule_block(x, dim, ks=3, s=1, name='res'):
+            p = int((ks - 1) / 2)
+            y = tf.pad(x, [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
+            y = instance_norm(conv2d(y, dim, ks, s, padding='VALID', name=name+'_c1'), name+'_bn1')
+            y = tf.pad(tf.nn.relu(y), [[0, 0], [p, p], [p, p], [0, 0]], "REFLECT")
+            y = instance_norm(conv2d(y, dim, ks, s, padding='VALID', name=name+'_c2'), name+'_bn2')
+            return y + x
+
+        # Justin Johnson's model from https://github.com/jcjohnson/fast-neural-style/
+        # The network with 9 blocks consists of: c7s1-32, d64, d128, R128, R128, R128,
+        # R128, R128, R128, R128, R128, R128, u64, u32, c7s1-3
+        #c0 = tf.pad(image, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT") # c0: 206,36,3
+        c0 = tf.pad(image, [[0, 0], [3, 3], [2, 3], [0, 0]], "REFLECT")  # c0: 
+        c1 = tf.nn.relu(instance_norm(conv2d(c0, options.gf_dim, 7, 1, padding='VALID', name='g_e1_c'), 'g_e1_bn'))
+        c2 = tf.nn.relu(instance_norm(conv2d(c1, options.gf_dim*2, 3, 2, name='g_e2_c'), 'g_e2_bn'))
+        c3 = tf.nn.relu(instance_norm(conv2d(c2, options.gf_dim*4, 3, 2, name='g_e3_c'), 'g_e3_bn'))
+        # define G network with 9 resnet blocks
+        r1 = residule_block(c3, options.gf_dim*4, name='g_r1')
+        r2 = residule_block(r1, options.gf_dim*4, name='g_r2')
+        r3 = residule_block(r2, options.gf_dim*4, name='g_r3')
+        r4 = residule_block(r3, options.gf_dim*4, name='g_r4')
+        r5 = residule_block(r4, options.gf_dim*4, name='g_r5')
+        r6 = residule_block(r5, options.gf_dim*4, name='g_r6')
+        r7 = residule_block(r6, options.gf_dim*4, name='g_r7')
+        r8 = residule_block(r7, options.gf_dim*4, name='g_r8')
+        r9 = residule_block(r8, options.gf_dim*4, name='g_r9')
+
+        d1 = deconv2d(r9, options.gf_dim*2, 3, 2, name='g_d1_dc')
+        d1 = tf.nn.relu(instance_norm(d1, 'g_d1_bn'))
+        d2 = deconv2d(d1, options.gf_dim, 3, 2, name='g_d2_dc')
+        d2 = tf.nn.relu(instance_norm(d2, 'g_d2_bn'))
+        #d2 = tf.pad(d2, [[0, 0], [3, 3], [3, 3], [0, 0]], "REFLECT")
+        d2 = tf.pad(d2, [[0, 0], [3, 3], [2, 2], [0, 0]], "REFLECT")
+        pred = tf.nn.tanh(conv2d(d2, options.output_c_dim, 7, 1, padding='VALID', name='g_pred_c'))
+
+        return pred
+
+        
+def generator_unet(image, options, reuse=False, name="generator"): #  not use this for default
+
+    dropout_rate = 0.5 if options.is_training else 1.0
+    with tf.variable_scope(name):
+        # image is 256 x 256 x input_c_dim
+        if reuse:
+            tf.get_variable_scope().reuse_variables()
+        else:
+            assert tf.get_variable_scope().reuse is False
+
+        # image is (256 x 256 x input_c_dim)
+        e1 = instance_norm(conv2d(image, options.gf_dim, name='g_e1_conv'))
+        # e1 is (128 x 128 x self.gf_dim)
+        e2 = instance_norm(conv2d(lrelu(e1), options.gf_dim*2, name='g_e2_conv'), 'g_bn_e2')
+        # e2 is (64 x 64 x self.gf_dim*2)
+        e3 = instance_norm(conv2d(lrelu(e2), options.gf_dim*4, name='g_e3_conv'), 'g_bn_e3')
+        # e3 is (32 x 32 x self.gf_dim*4)
+        e4 = instance_norm(conv2d(lrelu(e3), options.gf_dim*8, name='g_e4_conv'), 'g_bn_e4')
+        # e4 is (16 x 16 x self.gf_dim*8)
+        e5 = instance_norm(conv2d(lrelu(e4), options.gf_dim*8, name='g_e5_conv'), 'g_bn_e5')
+        # e5 is (8 x 8 x self.gf_dim*8)
+        e6 = instance_norm(conv2d(lrelu(e5), options.gf_dim*8, name='g_e6_conv'), 'g_bn_e6')
+        # e6 is (4 x 4 x self.gf_dim*8)
+        e7 = instance_norm(conv2d(lrelu(e6), options.gf_dim*8, name='g_e7_conv'), 'g_bn_e7')
+        # e7 is (2 x 2 x self.gf_dim*8)
+        e8 = instance_norm(conv2d(lrelu(e7), options.gf_dim*8, name='g_e8_conv'), 'g_bn_e8')
+        # e8 is (1 x 1 x self.gf_dim*8)
+
+        d1 = deconv2d(tf.nn.relu(e8), options.gf_dim*8, name='g_d1')
+        d1 = tf.nn.dropout(d1, dropout_rate)
+        d1 = tf.concat([instance_norm(d1, 'g_bn_d1'), e7], 3)
+        # d1 is (2 x 2 x self.gf_dim*8*2)
+
+        d2 = deconv2d(tf.nn.relu(d1), options.gf_dim*8, name='g_d2')
+        d2 = tf.nn.dropout(d2, dropout_rate)
+        d2 = tf.concat([instance_norm(d2, 'g_bn_d2'), e6], 3)
+        # d2 is (4 x 4 x self.gf_dim*8*2)
+
+        d3 = deconv2d(tf.nn.relu(d2), options.gf_dim*8, name='g_d3')
+        d3 = tf.nn.dropout(d3, dropout_rate)
+        d3 = tf.concat([instance_norm(d3, 'g_bn_d3'), e5], 3)
+        # d3 is (8 x 8 x self.gf_dim*8*2)
+
+        d4 = deconv2d(tf.nn.relu(d3), options.gf_dim*8, name='g_d4')
+        d4 = tf.concat([instance_norm(d4, 'g_bn_d4'), e4], 3)
+        # d4 is (16 x 16 x self.gf_dim*8*2)
+
+        d5 = deconv2d(tf.nn.relu(d4), options.gf_dim*4, name='g_d5')
+        d5 = tf.concat([instance_norm(d5, 'g_bn_d5'), e3], 3)
+        # d5 is (32 x 32 x self.gf_dim*4*2)
+
+        d6 = deconv2d(tf.nn.relu(d5), options.gf_dim*2, name='g_d6')
+        d6 = tf.concat([instance_norm(d6, 'g_bn_d6'), e2], 3)
+        # d6 is (64 x 64 x self.gf_dim*2*2)
+
+        d7 = deconv2d(tf.nn.relu(d6), options.gf_dim, name='g_d7')
+        d7 = tf.concat([instance_norm(d7, 'g_bn_d7'), e1], 3)
+        # d7 is (128 x 128 x self.gf_dim*1*2)
+
+        d8 = deconv2d(tf.nn.relu(d7), options.output_c_dim, name='g_d8')
+        # d8 is (256 x 256 x output_c_dim)
+
+        return tf.nn.tanh(d8)
+
+
+def abs_criterion(in_, target):
+    return tf.reduce_mean(tf.abs(in_ - target))
+
+
+def mae_criterion(in_, target):
+    return tf.reduce_mean((in_-target)**2)
+
+
+def sce_criterion(logits, labels):
+    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels))
+
+
+#----------------ops.py--------------------------------begin--------------
+import math
+import numpy as np
+
+import tensorflow.contrib.slim as slim
+from tensorflow.python.framework import ops
+
+#from utils import *
+
+def batch_norm(x, name="batch_norm"):
+    return tf.contrib.layers.batch_norm(x, decay=0.9, updates_collections=None, epsilon=1e-5, scale=True, scope=name)
+
+def instance_norm(input, name="instance_norm"):
+    with tf.variable_scope(name):
+        depth = input.get_shape()[3]
+        scale = tf.get_variable("scale", [depth], initializer=tf.random_normal_initializer(1.0, 0.02, dtype=tf.float32))
+        offset = tf.get_variable("offset", [depth], initializer=tf.constant_initializer(0.0))
+        mean, variance = tf.nn.moments(input, axes=[1,2], keep_dims=True)
+        epsilon = 1e-5
+        inv = tf.rsqrt(variance + epsilon)
+        normalized = (input-mean)*inv
+        return scale*normalized + offset
+
+def conv2d(input_, output_dim, ks=4, s=2, stddev=0.02, padding='SAME', name="conv2d"):
+    with tf.variable_scope(name):
+        return slim.conv2d(input_, output_dim, ks, s, padding=padding, activation_fn=None,
+                            weights_initializer=tf.truncated_normal_initializer(stddev=stddev),
+                            biases_initializer=None)
+
+def deconv2d(input_, output_dim, ks=4, s=2, stddev=0.02, name="deconv2d"):
+    with tf.variable_scope(name):
+        return slim.conv2d_transpose(input_, output_dim, ks, s, padding='SAME', activation_fn=None,
+                                    weights_initializer=tf.truncated_normal_initializer(stddev=stddev),
+                                    biases_initializer=None)
+
+def lrelu(x, leak=0.2, name="lrelu"):
+    return tf.maximum(x, leak*x)
+
+def linear(input_, output_size, scope=None, stddev=0.02, bias_start=0.0, with_w=False):
+
+    with tf.variable_scope(scope or "Linear"):
+        matrix = tf.get_variable("Matrix", [input_.get_shape()[-1], output_size], tf.float32,
+                                 tf.random_normal_initializer(stddev=stddev))
+        bias = tf.get_variable("bias", [output_size],
+            initializer=tf.constant_initializer(bias_start))
+        if with_w:
+            return tf.matmul(input_, matrix) + bias, matrix, bias
+        else:
+            return tf.matmul(input_, matrix) + bias
+
+#----------------ops.py--------------------------------end--------------
+
+
+#----------------utils.py--------------------------------begin--------------
+#from __future__ import division
+import math
+import pprint
+import scipy.misc
+#import numpy as np
+import copy
+
+class ImagePool(object):
+    def __init__(self, maxsize=50):
+        self.maxsize = maxsize
+        self.num_img = 0
+        self.images = []
+
+    def __call__(self, image):
+        if self.maxsize <= 0:
+            return image
+        if self.num_img < self.maxsize:
+            self.images.append(image)
+            self.num_img += 1
+            return image
+        if np.random.rand() > 0.5:
+            idx = int(np.random.rand()*self.maxsize)
+            tmp1 = copy.copy(self.images[idx])[0]
+            self.images[idx][0] = image[0]
+            idx = int(np.random.rand()*self.maxsize)
+            tmp2 = copy.copy(self.images[idx])[1]
+            self.images[idx][1] = image[1]
+            return [tmp1, tmp2]
+        else:
+            return image
+        
+        
+def save_images(images, size, image_path):
+    return imsave(inverse_transform(images), size, image_path)
+def imsave(images, size, path):
+    return scipy.misc.imsave(path, merge(images, size))
+def inverse_transform(images):
+    return (images+1.)/2.
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        img[j*h:j*h+h, i*w:i*w+w, :] = image
+
+    return img
+#----------------utils.py--------------------------------end--------------
